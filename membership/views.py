a94657cbfe1c5aab82fad35ce4f33efda48b102b
Patrick Pierson <patrick.c.pierson@gmail.com>
2018-10-17 18:52:46 -0400

    updates to app

--- membership/views.py
+++ membership/views.py
@@ -1,27 +1,11 @@
 from django.http import HttpResponse
-from membership.models import Member
-import csv
+from membership.UpdateDB import UpdateDB
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the membership index.")
 
 
 def update_db(request):
-    with open('/tmp/data/postquery.csv') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if 'Card_Number' not in row[0]:
-                print('Adding/updating member id %s' % row[0])
-                _, created = Member.objects.get_or_create(
-                    card_number=row[0],
-                    first_name=row[2],
-                    last_name=row[4],
-                    address=row[11],
-                    city=row[14],
-                    state=row[15],
-                    zipcode=row[16],
-                    membership_type=row[8],
-                    phone_number=row[19],
-                    email=row[20]
-                )
-    return HttpResponse('DB Updated')
+    updatedb = UpdateDB()
+    return HttpResponse('DB Updated with %s members' % updatedb.push_csv_to_db())
